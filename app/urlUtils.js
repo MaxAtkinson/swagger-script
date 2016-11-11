d98f19ba4aa3f773e0ff8b0204a62c8548f5947f
Max Atkinson <Max@Maxs-MacBook-Pro.local>
2016-11-11 11:36:50 +0000

    comment

--- app/urlUtils.js
+++ app/urlUtils.js
@@ -1,5 +1,9 @@
 import querystring from 'querystring';
 
+/*
+  Extracts path parameters from a url given in the form 'https://google.com/query/{key}'
+  and returns them in the swagger format.
+*/
 export function extractPathParams(endpoint) {
   return endpoint.match(/\{.*?\}/g).map((param) => {
     return {
@@ -11,6 +15,10 @@ export function extractPathParams(endpoint) {
   });
 }
 
+/*
+  Extracts querystring parameters from a url given in the form 'https://google.com/search?key=value'
+  and returns them in the swagger format.
+*/
 export function extractQueryParams(endpoint) {
   const queryParams = querystring.parse(endpoint.split('?')[1]);
   return Object.keys(queryParams).map((param) => {
